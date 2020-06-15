ff33e99f246374a59499841c12c867dfee6aecce
DAP <dlrayjr89@gmail.com>
2020-06-15 23:21:31 +0000

    searching good

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = (low + high) /2
+        if target < arr[int(middle)]:
+            high = middle -1
+        elif target > arr[int(middle)]:
+            low = middle - 1
+        else:
+            return middle
+
 
 
     return -1  # not found
